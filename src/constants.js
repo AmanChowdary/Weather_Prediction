a4548ca400404260b29820dad812322c48c33527
AmanChowdary <44564833+AmanChowdary@users.noreply.github.com>
2020-12-13 15:37:50 +0530

    project-update

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const API_KEY = "15fde6b035104ac79a39311df3f3f498";
+const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/current?city=Kiev,UA&key=${API_KEY}`;
+
+export { API_KEY, CURRENT_WEATHER };
